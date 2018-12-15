6f0cb51c9fc35d8863a7798dd3bd8d79e9035006
elizzzabethrae <elizabethwarners@gmail.com>
2018-12-15 12:36:00 -0500

    WIP deploy to production, attemt 2

--- src/server.js
+++ src/server.js
@@ -6,7 +6,8 @@ app.set("port", port);
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(process.env.PORT || 5000);
+//changed to fix heroku
 
 function normalizePort(val) {
   const port = parseInt(val, 10);
