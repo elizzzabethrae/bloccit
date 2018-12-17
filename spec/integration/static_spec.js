e8af3c85b7fb62235926b39b85d7d3bef79e2fc8
elizzzabethrae <elizabethwarners@gmail.com>
2018-12-16 22:39:39 -0500

    error Topic isn't defined

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -8,16 +8,15 @@ describe("routes : static", () => {
   describe("GET /", () => {
 
 //#2
-it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
 
 //#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
-
-
 //#4
-        done();
+        //done();
+        //commented out because it was throwing a 'not defined' error when testing
       });
     });
 
