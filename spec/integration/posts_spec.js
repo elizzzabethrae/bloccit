921dbe07d05107dbf9f02967e41c3e3c3e4c56d7
elizzzabethrae <elizabethwarners@gmail.com>
2018-12-22 13:02:49 -0500

    double checking views

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -42,7 +42,7 @@ describe("routes : posts", () => {
   describe("GET /topics/:topicId/posts/new", () => {
 
     it("should render a new post form", (done) => {
-      request.get(`${base}/${topic.id}/posts/new`, (err, res, body) => {
+      request.get(`${base}/${this.topic.id}/posts/new`, (err, res, body) => {
         expect(err).toBeNull();
         expect(body).toContain("New Post");
         done();
