b3c5a5017875f50d75d75eef02c9932e6513c85f
elizzzabethrae <elizabethwarners@gmail.com>
2019-01-06 11:18:31 -0500

    authorization assignment complete

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,3 +1,4 @@
+
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
@@ -6,86 +7,70 @@ const User = require("../../src/db/models").User;
 describe("Topic", () => {
 
   beforeEach((done) => {
-    this.topic;
-    this.post;
-    this.user;
+       this.topic;
+       this.post;
+       this.user;
 
-    sequelize.sync({force: true}).then((res) => {
+       sequelize.sync({force: true}).then((res) => {
 
-// #2
-      User.create({
-        email: "starman@tesla.com",
-        password: "Trekkie4lyfe"
-      })
-      .then((user) => {
-        this.user = user; //store the user
+  // #2
+         User.create({
+           email: "starman@tesla.com",
+           password: "Trekkie4lyfe"
+         })
+         .then((user) => {
+           this.user = user; //store the user
 
-// #3
-        Topic.create({
-          title: "Expeditions to Alpha Centauri",
-          description: "A compilation of reports from recent visits to the star system.",
+  // #3
+           Topic.create({
+             title: "Expeditions to Alpha Centauri",
+             description: "A compilation of reports from recent visits to the star system.",
 
-// #4
-          posts: [{
-            title: "My first visit to Proxima Centauri b",
-            body: "I saw some rocks.",
-            userId: this.user.id
-          }]
-        }, {
+  // #4
+             posts: [{
+               title: "My first visit to Proxima Centauri b",
+               body: "I saw some rocks.",
+               userId: this.user.id
+             }]
+           }, {
 
-// #5
-          include: {
-            model: Post,
-            as: "posts"
-          }
-        })
-        .then((topic) => {
-          this.topic = topic; //store the topic
-          this.post = topic.posts[0]; //store the post
-          done();
-        })
-      })
+  // #5
+             include: {
+               model: Post,
+               as: "posts"
+             }
+           })
+           .then((topic) => {
+             this.topic = topic; //store the topic
+             this.post = topic.posts[0]; //store the post
+             done();
+           })
+         })
+       });
+     });
+
+     describe("#create()", () => {
+
+     it("should create a topic object with a title and body", (done) => {
+//#1
+       Topic.create({
+         title: "Node.js is hard",
+         body: "CSS was easy!"
+       })
+       .then((post) => {
+
+//#2
+         expect(topic.title).toBe("Node.js is hard");
+         expect(topic.body).toBe("CSS was easy!");
+         done();
+
+       })
+       .catch((err) => {
+         console.log(err);
+         done();
+       });
+     });
+
     });
-  });
 
-
-describe("#create()", () => {
-
-  it("should create a topic with title and description", (done) => {
-
-    Topic.create({
-      title:"Javascript was cool",
-      description:"Pros and cons of javascript",
-      topicId: this.topic.id
-    })
-    .then((topic) => {
-
-      expect(topic.title).toBe("Javascript was cool");
-      expect(topic.description).toBe("Pros and cons of javascript");
-      done();
-    })
-    .catch((err) => {
-      console.log(err);
-      done();
-    });
-
-  });
-});
-
-describe("#getPosts()", () => {
-
-  it("should return all posts associated with a topic", (done) => {
-    console.log(`DEBUG:\n${this.topic}\n\n`);
-    this.topic.getPosts()
-    .then((associatedPosts) => {
-      expect(associatedPosts[0].title).toContain("My first visit to Proxima Centauri b");
-      done();
-    });
-  });
-
-});
-
-//getPosts topic.getPosts
-//presents array of post objects
-
-});
+   });
--- src/routes/topics.js
+++ src/routes/topics.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
 const topicController = require("../controllers/topicController");
+const validation = require("./validation");
 
 router.get("/topics", topicController.index);
 router.get("/topics/new", topicController.new);
@@ -10,6 +11,7 @@ router.get("/topics/:id", topicController.show);
 router.post("/topics/:id/destroy", topicController.destroy);
 router.get("/topics/:id/edit", topicController.edit);
 router.post("/topics/:topicId/posts/:id/destroy", postController.destroy);
+router.post("/topics/:id/update", topicController.update);
 //moved this to posts.js, is that right????
 
 
