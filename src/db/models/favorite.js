efd2c21170ba57ba0cb4ac4c87f1f4d551d5065a
elizzzabethrae <elizabethwarners@gmail.com>
2019-01-13 10:45:57 -0500

    error with showing favorites

--- src/db/models/favorite.js
+++ src/db/models/favorite.js
@@ -20,6 +20,16 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "userId",
       onDelete: "CASCADE"
     });
+    Favorite.addScope("lastFiveFor", (userId) => {
+      return {
+        include: [{
+          model: models.Post
+        }],
+        where: { userId: userId},
+        limit: 5,
+        order: [[ "createdAt", "DESC" ]]
+      }
+    });
   };
   return Favorite;
 };
