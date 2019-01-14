b0f5f215c2500883f4f4f82524df96a2e6889ead
elizzzabethrae <elizabethwarners@gmail.com>
2019-01-13 20:43:15 -0500

    trying to implement favorites scope on favorite model

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -3,6 +3,7 @@ const User = require("./models").User;
 const bcrypt = require("bcryptjs");
 const Post = require("./models").Post;
 const Comment = require("./models").Comment;
+const Favorite = require("./models").Favorite;
 
 module.exports = {
 // #2
@@ -46,6 +47,12 @@ module.exports = {
           .then((comments) => {
  // #7
             result["comments"] = comments;
+
+            Favorite.scope({method: ["lastFiveFor", id]}).all()
+            .then((favorites) => {
+
+              result["favorites"] = favorites;
+            })
             callback(null, result);
           })
           .catch((err) => {
