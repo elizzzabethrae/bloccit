43b9f16d4b3ec96da0d864435a802d441719d99f
elizzzabethrae <elizabethwarners@gmail.com>
2019-01-21 10:54:33 -0500

    make it better

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 const app = require("./app");
 const http = require("http");
+const server = http.createServer(app);
 
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
-
-const server = http.createServer(app);
 
 
 server.listen(port);
